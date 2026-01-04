743af4cc62d217e5ed2a7fef3e42d576c01e0a62
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-01-04 00:39:50 -0300

    Add Caneta class and main application logic in POO_aula4

---
+++ POO_aula4/POO_aula4.java
@@ -0,0 +1,19 @@
+package POO_aula4;
+
+public class POO_aula4 {
+    public static void main(String [] args){
+        Caneta c1 = new Caneta();
+        c1.setModelo("BIC");
+        c1.getModelo();
+        c1.setPonta(0.5f); // Nos set vc define os atributos
+        c1.getPonta();
+        c1.status();
+
+        System.out.println("************************************************************");
+        System.out.println("O modelo é " + c1.getModelo() + " e a ponta e " + c1.getPonta());
+        //Aqui nós só podemos escrever o private porque estamos usando o metodo, nao podemos escrever direto acessando o atributo
+
+
+
+    }
+}
