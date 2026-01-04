1d8e6f30a81df6a3358da6e5f3ac9de2ab333a77
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-01-04 00:58:00 -0300

    Refactor Caneta class to include constructor and additional attributes; update main application logic to reflect changes

--- POO_aula4/Caneta.java
+++ POO_aula4/Caneta.java
@@ -3,10 +3,17 @@ package POO_aula4;
 public class Caneta {
     public String modelo;
     private float ponta;
+    private boolean tampada;
+    private String cor;
 
+    public  Caneta(String m, String c, float p){ //metodo construtor, nao tem tipo, é só public e o nome da classe
+        this.modelo = m;
+        this.cor = c;
+        this.ponta = p;
+        this.tampar();
+    }
     
-
-    public String getModelo(){
+    /*public String getModelo(){
         return this.modelo;
     }
     public void setModelo(String m){
@@ -17,12 +24,22 @@ public class Caneta {
     }
     public void setPonta(float p){
         this.ponta = p;
+    }*/
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
     }
 
     public void status(){
         System.out.println("SOBRE A CANETA: ");
                 System.out.println("Modelo " + this.modelo);
                         System.out.println("Ponta " + this.ponta);
+                            System.out.println("Cor " + this.cor);
+                                System.out.println("Tampada: " + this.tampada);
 
 
     }
--- POO_aula4/POO_aula4.java
+++ POO_aula4/POO_aula4.java
@@ -2,8 +2,14 @@ package POO_aula4;
 
 public class POO_aula4 {
     public static void main(String [] args){
-        Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
+        Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);
+        c1.status();
+
+
+
+
+
+        /*c1.setModelo("BIC");
         c1.getModelo();
         c1.setPonta(0.5f); // Nos set vc define os atributos
         c1.getPonta();
@@ -12,7 +18,7 @@ public class POO_aula4 {
         System.out.println("************************************************************");
         System.out.println("O modelo é " + c1.getModelo() + " e a ponta e " + c1.getPonta());
         //Aqui nós só podemos escrever o private porque estamos usando o metodo, nao podemos escrever direto acessando o atributo
-
+        */
 
 
     }
