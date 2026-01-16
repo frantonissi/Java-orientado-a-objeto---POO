c1f78dec39264a6362d2b38bb0fd87234319dd3e
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-01-16 00:00:26 -0300

    Implement fight management methods in Luta class; add marcarLuta and lutar methods for match setup and execution

--- POO_aula7/Luta.java
+++ POO_aula7/Luta.java
@@ -1,11 +1,14 @@
 package POO_aula7;
 
+import java.util.Random;
+
 public class Luta {
     private Lutador desafiante;
     private Lutador desafiado;
     private int rounds;
     private boolean aprovada;
 
+    //Metodos especiais:
     public Lutador getDesafiante(){
         return desafiante;
     }
@@ -39,5 +42,45 @@ public class Luta {
     }
 
     //////////////////////////////////////////////
+    
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if(l1.getCategorias().equals(l2.getCategorias()) && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        }else{
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+    }
+
+    public void lutar(){
+        if(this.aprovada){
+            desafiado.apresentar();
+            desafiante.apresentar();
+            Random aleatorio  = new Random();
+            int vencedor = aleatorio.nextInt(3); //vai gerar 3 aleatorios
+            switch (vencedor) {
+                case 0:
+                    System.out.println("Empatou");
+                    desafiado.empatarLuta();
+                    desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println(desafiado.getNome() + "ganhou a luta!");
+                    desafiado.ganharLuta();
+                    desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println(desafiante.getNome() + "ganhou a luta");
+                    desafiado.perderLuta();
+                    desafiante.ganharLuta();
+            
+                default:
+                    break;
+            }
+        }
+    }
     
 }
