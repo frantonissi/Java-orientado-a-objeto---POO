70fb1956f479e79e0fc6873d223edbcecceda900
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-02-09 18:22:52 -0300

    Instantiate Cachorro class and invoke reagir method in main for demonstration

--- POO_aula13/Main.java
+++ POO_aula13/Main.java
@@ -4,6 +4,8 @@ package POO_aula13;
 //Aula sobre Polimorfismo de sobrecarga!
 public class Main {
     public static void main(String[] args) {
-        
+        Cachorro c = new Cachorro();
+
+        c.reagir(false);
     }
 }
