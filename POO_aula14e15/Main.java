c910e6498794bf61e5e0afd29eb2705a04dd7d9b
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-02-20 17:57:23 -0300

    Commit aleatorio pois o gemini nao ta funcionando

--- POO_aula14e15/Main.java
+++ POO_aula14e15/Main.java
@@ -7,6 +7,13 @@ public class Main {
         v[1] = new Video("Aprendendo Ciberseguranca");
         v[2] = new Video("Aprendendo Java");
 
-        v[2].toString();
+        v[2].like();
+        v[2].play();
+        System.out.println(v[2].toString());
+
+        Gafanhoto g[] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Francisco", 19, "Masculino", "Francis");
+        g[1] = new Gafanhoto("Laura", 33, "Feminino", "Laurinha123");
+        System.out.println(g[0].toString());
     }
 }
